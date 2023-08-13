d18cc16eb096033054f382e374bc466a44516c89
Bálint Csertán <csertanb@gmail.com>
2023-08-13 23:55:47 +0200

    add amount setup

--- drink-water/script.js
+++ drink-water/script.js
@@ -1,16 +1,40 @@
-const smallCups = document.querySelectorAll('.cup-small');
+let smallCups = document.querySelectorAll('.cup-small');
+const cupsContainer = document.querySelector('.cups');
 const listers = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
+const waterToDrinkInput = document.getElementById('waterToDrink');
+let waterToDrink = waterToDrinkInput.value;
+const setupAmount = document.getElementById('setupAmount');
+const oneCupAmount = 0.250;
 
 updateBigCup();
+
+setupAmount.addEventListener('click', () => {
+    waterToDrink = waterToDrinkInput.value;
+    cupsContainer.innerHTML = '';
+    for (let i = 0; i < waterToDrink; i++) {
+        for (let j = 0; j < (1 / oneCupAmount); j++) {
+            cupsContainer.innerHTML += '<div class="cup cup-small">250 ml</div>';
+        }
+    }
+    setupCups();
+});
+
+function setupCups() {
+    smallCups = document.querySelectorAll('.cup-small');
+    smallCups.forEach((cup, idx) => {
+        cup.addEventListener('click', () => highlightCups(idx));
+    });
+}
 
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx));
 });
 
 function highlightCups(idx) {
-    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+    if (smallCups[idx].classList.contains('full') &&
+        !smallCups[idx].nextElementSibling.classList.contains('full')) {
         idx--;
     }
 
@@ -35,7 +59,7 @@ function updateBigCup() {
     } else {
         percentage.style.visibility = 'visible';
         percentage.style.height = `${fullCups / totalCups * 330}px`;
-        percentage.innerText = `${fullCups / totalCups * 100}%`
+        percentage.innerText = `${Math.round((fullCups / totalCups * 100) * 100) / 100}%`
     }
 
     if (fullCups === totalCups) {
@@ -43,6 +67,6 @@ function updateBigCup() {
         remained.style.height = 0;
     } else {
         remained.style.visibility = 'visible';
-        listers.innerText = `${2 - (250 * fullCups / 1000)}L`;
+        listers.innerText = `${waterToDrink - (250 * fullCups / 1000)}L`;
     }
 }
